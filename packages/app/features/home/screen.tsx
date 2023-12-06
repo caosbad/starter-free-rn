2440e1107a215042f2819d4f9292da1f1e4d9a59
Ranger <43808588+RangerX42@users.noreply.github.com>
2023-12-06 21:26:36 +0800

    update file

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -4,19 +4,36 @@ import {
   H1,
   Paragraph,
   Separator,
-  Sheet,
-  useToastController,
+  // Sheet,
+  // useToastController,
   XStack,
   YStack,
 } from '@my/ui'
-import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
+// import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
+import * as Updates from 'expo-updates'
+
 import React, { useState } from 'react'
+
 import { useLink } from 'solito/link'
 
 export function HomeScreen() {
   const linkProps = useLink({
     href: '/user/nate',
   })
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync()
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync()
+        await Updates.reloadAsync()
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`)
+    }
+  }
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
@@ -47,52 +64,53 @@ export function HomeScreen() {
 
       <XStack>
         <Button {...linkProps}>Link to user</Button>
+        <Button onPress={onFetchUpdateAsync}>Link to user</Button>
       </XStack>
 
-      <SheetDemo />
+      {/* <SheetDemo /> */}
     </YStack>
   )
 }
 
-function SheetDemo() {
-  const [open, setOpen] = useState(false)
-  const [position, setPosition] = useState(0)
-  const toast = useToastController()
+// function SheetDemo() {
+//   const [open, setOpen] = useState(false)
+//   const [position, setPosition] = useState(0)
+//   const toast = useToastController()
 
-  return (
-    <>
-      <Button
-        size="$6"
-        icon={open ? ChevronDown : ChevronUp}
-        circular
-        onPress={() => setOpen((x) => !x)}
-      />
-      <Sheet
-        modal
-        animation="medium"
-        open={open}
-        onOpenChange={setOpen}
-        snapPoints={[80]}
-        position={position}
-        onPositionChange={setPosition}
-        dismissOnSnapToBottom
-      >
-        <Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
-        <Sheet.Frame ai="center" jc="center">
-          <Sheet.Handle />
-          <Button
-            size="$6"
-            circular
-            icon={ChevronDown}
-            onPress={() => {
-              setOpen(false)
-              toast.show('Sheet closed!', {
-                message: 'Just showing how toast works...',
-              })
-            }}
-          />
-        </Sheet.Frame>
-      </Sheet>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <Button
+//         size="$6"
+//         icon={open ? ChevronDown : ChevronUp}
+//         circular
+//         onPress={() => setOpen((x) => !x)}
+//       />
+//       <Sheet
+//         modal
+//         animation="medium"
+//         open={open}
+//         onOpenChange={setOpen}
+//         snapPoints={[80]}
+//         position={position}
+//         onPositionChange={setPosition}
+//         dismissOnSnapToBottom
+//       >
+//         <Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
+//         <Sheet.Frame ai="center" jc="center">
+//           <Sheet.Handle />
+//           <Button
+//             size="$6"
+//             circular
+//             icon={ChevronDown}
+//             onPress={() => {
+//               setOpen(false)
+//               toast.show('Sheet closed!', {
+//                 message: 'Just showing how toast works...',
+//               })
+//             }}
+//           />
+//         </Sheet.Frame>
+//       </Sheet>
+//     </>
+//   )
+// }
